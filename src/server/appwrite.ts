a8eac666d6e1f30bd9bfafc5d0ca27ba85bf2182
PartyGodTroy <kidveno@live.com>
2024-12-24 15:17:26 -0500

    login

--- src/server/appwrite.ts
+++ src/server/appwrite.ts
@@ -1,10 +1,10 @@
 import sdk from "node-appwrite";
 
 let client = new sdk.Client();
-
 client
     .setEndpoint(`${process.env._APP_DOMAIN}/v1`) // Your API Endpoint
     .setProject(`${process.env._STIGMA_APPWRITE_PROJECT_ID}`) // Your project ID
     .setKey(`${process.env._STIGMA_SECRET_KEY}`) // Your secret API key
     .setSelfSigned(process.env.NODE_ENV !== "production") // Use only on dev mode with a self-signed SSL cert
 ;
+
